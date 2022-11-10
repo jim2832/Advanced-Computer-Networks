eba4b4596ae02011a59e388ce90b5fc75e8ffa54
Jim Lee <jim880705@gmail.com>
2022-11-10 19:20:02 +0800

    update

--- HW6/main.c
+++ HW6/main.c
@@ -15,6 +15,36 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+uint16_t calculate_checksum(unsigned char* buffer, int bytes){
+    uint32_t checksum = 0;
+    unsigned char* end = buffer + bytes;
+
+    // odd bytes add last byte and reset end
+    if (bytes % 2 == 1) {
+        end = buffer + bytes - 1;
+        checksum += (*end) << 8;
+    }
+
+    // add words of two bytes, one by one
+    while (buffer < end) {
+        checksum += buffer[0] << 8;
+        checksum += buffer[1];
+        buffer += 2;
+    }
+
+    // add carry if any
+    uint32_t carray = checksum >> 16;
+    while (carray) {
+        checksum = (checksum & 0xffff) + carray;
+        carray = checksum >> 16;
+    }
+
+    // negate it
+    checksum = ~checksum;
+
+    return checksum & 0xffff;
+}
+
 int main(int argc, char **argv){
     int sockfd_recv = 0, sockfd_send = 0;
 
