1abb900e776b3d5cb11939f0735be6a9135d234a
jim2832 <jim880705@gmail.com>
2022-11-11 06:50:41 +0800

    221110 update

--- HW6/main.c
+++ HW6/main.c
@@ -36,7 +36,7 @@ unsigned short checksum(unsigned short *buf, int bufsz){
     return ~sum;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char **argv){
     int soc;
     struct icmphdr hdr;
     struct sockaddr_in addr;
@@ -44,21 +44,21 @@ int main(int argc, char *argv[]){
     char buf[1024];
     struct icmphdr *icmphdrptr;
     struct iphdr *iphdrptr;
-    int max_hopping = argv[1];
+    int TTL = 0;
+    int max_hopping = atoi(argv[1]);
     char target_addr[32];
     memcpy(target_addr, argv[2], 32);
-
 
     addr.sin_family = PF_INET; // IPv4
 
     // 將使用者輸入的 IP 轉成 network order
-    network_number = inet_pton(PF_INET, argv[1], &addr.sin_addr);
+    network_number = inet_pton(PF_INET, argv[2], &addr.sin_addr);
     if(network_number < 0){
         perror("inet_pton");
         exit(-1);
     }
 
-    // 開一個 IPv4 的 RAW Socket , 並且準備收取 ICMP 封包
+    //創建一個socket收取ICMP封包
     soc = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
     if(soc < 0){
         perror("socket");
@@ -74,60 +74,61 @@ int main(int argc, char *argv[]){
     hdr.checksum = 0;
     hdr.un.echo.id = 0;
     hdr.un.echo.sequence = 0;
+    hdr.checksum = checksum((unsigned short*)&hdr, sizeof(hdr)); //計算checksum
 
-    // 計算出 checksum
-    hdr.checksum = checksum((unsigned short*)&hdr, sizeof(hdr));
+    for(TTL=1; TTL<max_hopping; TTL++){
+        printf("Now the TTL is %d\n", TTL);
+        // 將定義好的 ICMP Header 送到目標主機
+        network_number = sendto(soc, (char*)&hdr, sizeof(hdr), 0, (struct sockaddr*)&addr, sizeof(addr));
+        if(network_number < 1){
+            perror("sendto");
+            exit(-1);
+        }
+        printf("an ICMP packet has been sent to %s\n", argv[2]);
 
-    // 將定義好的 ICMP Header 送到目標主機
-    network_number = sendto(soc, (char*)&hdr, sizeof(hdr), 0, (struct sockaddr*)&addr, sizeof(addr));
-    if(network_number < 1){
-        perror("sendto");
-        exit(-1);
+        // 清空 buf
+        memset(buf, 0, sizeof(buf));
+
+        printf("Waiting for ICMP echo reply...\n");
+
+        // 接收來自目標主機的 Echo Reply
+        network_number = recv(soc, buf, sizeof(buf), 0);
+        if(network_number < 1){
+            perror("recv");
+            exit(-1);
+        }
+
+        // 取出 IP Header
+        iphdrptr = (struct iphdr*)buf;
+
+        // 取出 ICMP Header
+        icmphdrptr = (struct icmphdr*)(buf+(iphdrptr->ihl)*4);
+
+        // 判斷 ICMP 種類
+        switch(icmphdrptr->type){
+            case 3:
+                printf("address %s: Destination Unreachable\n", argv[2]);
+                printf("The ICMP type is %d\n", icmphdrptr->type);
+                printf("The ICMP code is %d\n", icmphdrptr->code);
+                break;
+            case 8:
+                printf("The host %s is alive!\n", argv[2]);
+                printf("The ICMP type is %d\n", icmphdrptr->type);
+                printf("The ICMP code is %d\n", icmphdrptr->code);
+                break;
+            case 0:
+                printf("The host %s is alive!\n", argv[2]);
+                printf("The ICMP type is %d\n", icmphdrptr->type);
+                printf("The ICMP code is %d\n", icmphdrptr->code);
+                break;
+            default:
+                printf("Another situations!\n");
+                printf("The ICMP type is %d\n", icmphdrptr->type);
+                printf("The ICMP code is %d\n", icmphdrptr->code);
+                break;
+        }
+        printf("\n");
+        }
+        close(soc); // 關閉 socket
+        return EXIT_SUCCESS;
     }
-    printf("We have sended an ICMP packet to %s\n", argv[1]);
-
-    // 清空 buf
-    memset(buf, 0, sizeof(buf));
-
-    printf("Waiting for ICMP echo...\n");
-
-    // 接收來自目標主機的 Echo Reply
-    network_number = recv(soc, buf, sizeof(buf), 0);
-    if(network_number < 1){
-        perror("recv");
-        exit(-1);
-    }
-
-    // 取出 IP Header
-    iphdrptr = (struct iphdr*)buf;
-
-    // 取出 ICMP Header
-    icmphdrptr = (struct icmphdr*)(buf+(iphdrptr->ihl)*4);
-
-    // 判斷 ICMP 種類
-    switch(icmphdrptr->type){
-        case 3:
-            printf("The host %s is a unreachable purpose!\n", argv[1]);
-            printf("The ICMP type is %d\n", icmphdrptr->type);
-            printf("The ICMP code is %d\n", icmphdrptr->code);
-            break;
-        case 8:
-            printf("The host %s is alive!\n", argv[1]);
-            printf("The ICMP type is %d\n", icmphdrptr->type);
-            printf("The ICMP code is %d\n", icmphdrptr->code);
-            break;
-        case 0:
-            printf("The host %s is alive!\n", argv[1]);
-            printf("The ICMP type is %d\n", icmphdrptr->type);
-            printf("The ICMP code is %d\n", icmphdrptr->code);
-            break;
-        default:
-            printf("Another situations!\n");
-            printf("The ICMP type is %d\n", icmphdrptr->type);
-            printf("The ICMP code is %d\n", icmphdrptr->code);
-            break;
-    }
-
-    close(soc); // 關閉 socket
-    return EXIT_SUCCESS;
-}
